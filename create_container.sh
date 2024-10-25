ec162c06a89e0893101b019d31eeff4a21098f11
gsarav <107950808+gsarav@users.noreply.github.com>
2024-10-24 23:26:06 -0400

    add configurations to each type of honeypot

--- create_container.sh
+++ create_container.sh
@@ -11,9 +11,9 @@
 # 7. Stopping and destroying container
 
 # Checking that there are IP addresses to create containers for
-if [[ ! -s "free_ip_addr.txt" ]]
+if [[ ! -s "./free_ip_addr.txt" ]]
 then
-  echo "usage: 'free_ip_addr.txt' is empty-- please add valid IP addresses to file"
+  echo "usage: './free_ip_addr.txt' is empty-- please add valid IP addresses to file"
   exit 1
 fi
 
@@ -44,57 +44,101 @@ do
   then
     container_name="nobanner-nosession-" # empty container
 
-fi
+  fi
 
-# Generating a random ID number for the container name
-  container_name+=$(shuf -i 1-100000 -n 1)
+# Generating an ID number for the container name based on the IP address
+  id=$(( $(tail -1 "id.txt") + 1 ))
+  echo "$id" >> "id.txt"
+  container_name="${container_name}${id}"
+  echo "Container Name: $container_name"
 
-
-# Creating the LXC container with $container_type
-   sudo lxc-create -n "$container_name" -t download -- -d ubuntu -r focal -a amd64
+# Creating the LXC container with container_type
+  sudo lxc-create -n "$container_name" -t download -- -d ubuntu -r focal -a amd64
 # Wait 15 seconds for the container to start
-  sleep 15
+  sleep 25
 
   sudo lxc-start -n "$container_name" # Start the container
-  sleep 2 # Wait two seconds to start the container
+  sleep 5 # Wait two seconds to start the container
 
-  container_ip=$(sudo lxc-info -n "$container_name" | grep "IP" | xargs | cut -d" " -f2) # Getting new container's IP address
-  external_ip=$($ip_addr) # External IP address that will route to the new container
+  container_ip=$(sudo lxc-info -i -H $container_name)
+  external_ip="$ip_addr" # External IP address that will route to the new container
+  echo "Container IP = $container_ip"
+  echo "External IP = $external_ip"
 
 # ========= CONFIGURE THE HONEYPOT =======
 # Example: ./populate.sh "$container_name"
 # 1. Copy files from VM to container depending on container type
-# 2. Poison commands >> wget, curl, exit, logout 
-# 3. Install up ssh on container
+# 2. Poison commands >> wget, curl, exit, logout
+# 3. Install open ssh on container
 #
-#
-# 
+  sudo lxc-attach -n "$container_name" -- bash -c "sudo apt-get update && sudo apt-get install openssh-server -y" 
+  sudo lxc-attach -n "$container_name" -- bash -c "sudo apt-get install -y curl"
+  #sudo lxc-attach -n "$container_name" -- bash -c "curl -fsSl https://ollama.com/install.sh | sh" 
+  #sudo lxc-attach -n "$container_name" -- bash -c "ollama pull qwen:0.5b-chat-v1.5-q2_K"
+
+  #if [[ "$random_number" -eq 1 ]]
+  #then
+  #  ./NEWCONFIGMANAGER 1 $container_name
+  #elif [[ "$random_number" -eq 2 ]]
+  #then
+  #  ./NEWCONFIGMANAGER 2 $container_name
+  #elif [[ "$random_number" -eq 3 ]]
+  #then
+  #   ./NEWCONFIGMANAGER 3 $container_name
+  # elif [[ "$random_number" -eq 4 ]]
+  # then
+  #   ./NEWCONFIGMANAGER 4 $container_name
+  #fi
+
+  echo "NEWCONFIGMANAGER run"
+
+  #sudo lxc-attach -n "$container_name" -- bash -c "sudo apt-get update && sudo apt-get install openssh-server -y" 
+
 # ==== SETTING UP NAT RULES ====
-  sudo ip addr add "$external_ip"/16 brd + dev eth1
-  sudo ip link set dev eth1 up
-  sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination "$external_ip" --protocol tcp --dport 22 --jump DNAT --to-destination "$container_ip"
+  sudo ip addr add "$external_ip"/24 brd + dev eth3
+  # sudo ip link set dev eth3 up
+  sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination "$external_ip" --jump DNAT --to-destination "$container_ip"
   sudo iptables --table nat --insert POSTROUTING --source "$container_ip" --destination 0.0.0.0/0 --jump SNAT --to-source "$external_ip"
+  echo "NAT setup" 
 
 # ==== SETTING UP MITM ====
-# Generate a random port to open the MITM server
-# Check if the port is in use, if it is, generate a new port
-#  sudo sysctl -w net.ipv4.conf.all.route_localnet=1
-#  MITM_port=$(shuf -i 1024-65535 -n 1) <---- MUST CHECK IF PORT IS ALREADY IN USE
-#
-#  sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination "$external_ip" --protocol tcp --dport 22 --jump DNAT --to-destination 127.0.0.1:"$MITM_port"
-#  sudo lxc-attach -n "$container_name" -- sudo apt-get install openssh-server -y
-#  sudo npm install -g forever
-#  sudo forever -l ~/mitm_logs/"$container_name".log start node mitm.js -n "$container_name" -i "$container_ip" -p "$MITM_port" --auto-access --auto-access-fixed 1 --debug
+  MITM_port=$id # using port equal to the container id
+  sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination "$external_ip" --protocol tcp --dport 22 --jump DNAT --to-destination 10.0.3.1:"$MITM_port"
+  sudo sysctl -w net.ipv4.conf.all.route_localnet=1
+  
+  echo "MITM setup"
+
+  # sudo npm install -g forever
+  log_path=""
+  if [[ "$random_number" -eq 1 ]]
+  then
+    log_path="banner-session-logs"
+  elif [[ "$random_number" -eq 2 ]]
+  then
+      log_path="banner-nosession-logs"
+  elif [[ "$random_number" -eq 3 ]]
+  then
+     log_path="nobanner-session-logs"
+   elif [[ "$random_number" -eq 4 ]]
+   then
+     log_path="nobanner-nosession-logs"
+  fi
+
+  # touch ~/mitm_logs/"$log_path"/"$container_name".log
+  sudo forever -l ~/mitm_logs/"$log_path"/"$container_name".log start -a /home/student/MITM/mitm.js -n "$container_name" -i "$container_ip" -p "$MITM_port" --mitm-ip 10.0.3.1 --auto-access --auto-access-fixed 1 --debug
 
 #
 #
 #
 # ==== MONITORING TO CHECK FOR SSH CONNECTION  ====
-  ./monitor.sh "$container_name" & # The script stops running once "break" runs in the script
+   ./monitor.sh "$container_name" "$external_ip" & 
+   echo $!
+   monitor_pid=$!
+   echo "$monitor_pid" > "./monitoring_${id}"
 
 #
 # ==== REMOVE IP ADDRESS FROM  "free_ip_addr.txt"
-  sed -i '/"$ip_addr"/d' "free_ip_addr.txt"
+  sed -i "/$ip_addr/d" "./free_ip_addr.txt"
 
 done # Ending for loop to create a container for each IP address
 
