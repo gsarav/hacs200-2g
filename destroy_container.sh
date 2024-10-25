af3da868887d7e499c679cce7e8f2a60e0153cfb
gsarav <107950808+gsarav@users.noreply.github.com>
2024-10-24 23:26:58 -0400

    update destroy_container.sh

--- destroy_container.sh
+++ destroy_container.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
-# Steps for Destroying a Container
-# 1. Removing NAT rules
-# 2. Stopping and destroying the container
-#
-
 if [[ $# -ne 2 ]];
 then
   echo "usage: ./destroy_container.sh [container_name] [external ip]"
   exit 1
 fi
 
+container_name=$1
 container_ip=$(sudo lxc-info -n "$1" | grep "IP" | xargs | cut -d" " -f2)
 external_ip=$2
+MITM_port=$(echo "$1" | cut -d"-" -f3)
+
+MITM_pid=$(sudo forever list 2>/dev/null | awk -v port="$MITM_port" '$0 ~ port {getline; print $3}')
+sudo forever stop "$MITM_pid"
+
+id=$(echo "$container_name" | cut -d"-" -f3)
+monitor_pid=$(cat "./monitoring_${id}")
+kill $monitor_pid
+rm "./monitoring_${id}"
+
 
 # ==== DELETE MITM PREROUTING AND NAT RULES ====
-sudo iptables --table nat --delete PREROUTING --source 0.0.0.0/0 --destination "$external_ip" --protocol tcp --dport 22 --jump DNAT --to-destination "$container_ip"
+sudo iptables --table nat --delete PREROUTING --source 0.0.0.0/0 --destination "$external_ip" --jump DNAT --to-destination "$container_ip"
 sudo iptables --table nat --delete POSTROUTING --source "$container_ip" --destination 0.0.0.0/0 --jump SNAT --to-source "$external_ip"
-# delete MITM PREROUTING rule
-#
+
+# Delete MITM PREROUTING rule
+sudo iptables --table nat --delete PREROUTING --source 0.0.0.0/0 --destination "$external_ip" --protocol tcp --dport 22 --jump DNAT --to-destination 10.0.3.1:"$MITM_port"
+
+
+
 # ==== DELETE THE CONTAINER ====
-sudo ip addr delete "$external_ip"/16 brd + dev eth1
+sudo ip addr delete "$external_ip"/24 brd + dev eth3
 sudo lxc-stop -n "$1"
 sleep 3
 sudo lxc-destroy -n "$1"
