7786982e0bfdaa51259f3d3c96b3810b83bedddc
gsarav <107950808+gsarav@users.noreply.github.com>
2024-10-29 19:12:01 -0400

    Update reboot.sh
    Adding current working IPs

--- reboot.sh
+++ reboot.sh
@@ -2,13 +2,10 @@
 modprobe br_netfilter
 sysctl -p /etc/sysctl.conf
 /bin/bash /home/student/firewall_rules.sh
-#echo "firewall" > /home/student/test1
 sudo ip link set dev eth3 up
-# rm /home/student/free_ip_addr.txt
-# echo "128.8.238.129" > /home/student/free_ip_addr.txt
-# echo "128.8.238.90" >> /home/student/free_ip_addr.txt
-# echo "128.8.238.179" >> /home/student/free_ip_addr.txt
+
 /home/student/create_container.sh 128.8.238.129
 /home/student/create_container.sh 128.8.238.90
-
-# sudo /home/student/create_container.sh
+/home/student/create_container.sh 128.8.238.141
+/home/student/create_container.sh 128.8.238.159
+/home/student/create_container.sh 128.8.238.137
