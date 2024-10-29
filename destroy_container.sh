5c5c29042e77a3106623b39daac9e4fdea5eccf5
gsarav <107950808+gsarav@users.noreply.github.com>
2024-10-29 16:13:59 -0400

    Update destroy_container.sh
     Updating parameters to create_container.sh and stopping specific forever process.

--- destroy_container.sh
+++ destroy_container.sh
@@ -2,23 +2,18 @@
 
 if [[ $# -ne 2 ]];
 then
-  echo "usage: ./destroy_container.sh [container_name] [external ip]"
+  echo "usage: ./destroy_container.sh [container_name] [external_ip]"
   exit 1
 fi
 
-container_name=$1
 container_ip=$(sudo lxc-info -n "$1" | grep "IP" | xargs | cut -d" " -f2)
 external_ip=$2
 MITM_port=$(echo "$1" | cut -d"-" -f3)
 
-MITM_pid=$(sudo forever list 2>/dev/null | awk -v port="$MITM_port" '$0 ~ port {getline; print $3}')
+container_name=$1
+
+MITM_pid=$(sudo forever list 2>/dev/null | grep "$container_name" | xargs | cut -d" " -f19)
 sudo forever stop "$MITM_pid"
-
-id=$(echo "$container_name" | cut -d"-" -f3)
-monitor_pid=$(cat "./monitoring_${id}")
-kill $monitor_pid
-rm "./monitoring_${id}"
-
 
 # ==== DELETE MITM PREROUTING AND NAT RULES ====
 sudo iptables --table nat --delete PREROUTING --source 0.0.0.0/0 --destination "$external_ip" --jump DNAT --to-destination "$container_ip"
@@ -26,8 +21,6 @@ sudo iptables --table nat --delete POSTROUTING --source "$container_ip" --destin
 
 # Delete MITM PREROUTING rule
 sudo iptables --table nat --delete PREROUTING --source 0.0.0.0/0 --destination "$external_ip" --protocol tcp --dport 22 --jump DNAT --to-destination 10.0.3.1:"$MITM_port"
-
-
 
 # ==== DELETE THE CONTAINER ====
 sudo ip addr delete "$external_ip"/24 brd + dev eth3
@@ -37,7 +30,7 @@ sudo lxc-destroy -n "$1"
 sleep 3
 
 # ==== ADD THE NOW FREE IP ADDRESS BACK TO free_ip_addr.txt
-echo "$external_ip" >> free_ip_addr.txt
+#e cho "$external_ip" >> free_ip_addr.txt
 # Call create_container.sh to create a new container
 
-#./create_container.sh
+/home/student/create_container.sh "$external_ip"
